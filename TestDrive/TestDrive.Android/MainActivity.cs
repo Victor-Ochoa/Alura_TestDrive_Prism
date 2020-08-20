25cdb5cb02f58c92c5bb84efb07982130e680d04
Victor Ochoa <Victor.Ochoa@POSSIBLE.com>
2020-08-20 16:56:43 -0300

    add a plugin Xam.Plugin.Media

--- TestDrive/TestDrive.Android/MainActivity.cs
+++ TestDrive/TestDrive.Android/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Plugin.Media;
 using Prism;
 using Prism.Ioc;
 
@@ -9,13 +10,14 @@ namespace TestDrive.Droid
     [Activity(Label = "TestDrive", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override async void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
 
+            await CrossMedia.Current.Initialize();
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App(new AndroidInitializer()));
