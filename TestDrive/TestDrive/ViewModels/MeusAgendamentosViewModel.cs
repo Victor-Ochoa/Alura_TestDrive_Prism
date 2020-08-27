d6731b92bfc8b788a54fa5186424ff652c891c33
Victor Ochoa <Victor.Ochoa@POSSIBLE.com>
2020-08-27 09:37:52 -0300

    adicionado a logica para reenvio das informações de agendamento

--- TestDrive/TestDrive/ViewModels/MeusAgendamentosViewModel.cs
+++ TestDrive/TestDrive/ViewModels/MeusAgendamentosViewModel.cs
@@ -2,11 +2,13 @@
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using Prism.Services;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows.Input;
+using TestDrive.Interfaces;
 using TestDrive.Models;
 
 namespace TestDrive.ViewModels
@@ -14,44 +16,91 @@ namespace TestDrive.ViewModels
     public class MeusAgendamentosViewModel : Core.ViewModelBase
     {
         private readonly ILiteDatabase _liteDatabase;
+        private readonly IPageDialogService _pageDialogService;
+        private readonly IAgendamentoService _agendamentoService;
 
-        private DelegateCommand<Agendamento> _agendamentoTappedCommand;
-        public DelegateCommand<Agendamento> AgendamentoTappedCommand =>
-            _agendamentoTappedCommand ?? (_agendamentoTappedCommand = new DelegateCommand<Agendamento>(ExecuteAgendamentoTappedCommand));
-
-        void ExecuteAgendamentoTappedCommand(Agendamento agendamento)
-        {
-
-        }
-
-        public ObservableCollection<Agendamento> MeusAgendamentos { get; set; }
-
-        public MeusAgendamentosViewModel(INavigationService navigationService, ILiteDatabase liteDatabase)
+        public MeusAgendamentosViewModel(INavigationService navigationService, ILiteDatabase liteDatabase, IPageDialogService pageDialogService, IAgendamentoService agendamentoService)
             : base(navigationService)
         {
             this._liteDatabase = liteDatabase;
-
+            this._pageDialogService = pageDialogService;
+            this._agendamentoService = agendamentoService;
             MeusAgendamentos = new ObservableCollection<Agendamento>();
         }
+
+
+        private DelegateCommand _agendamentoTappedCommand;
+        public DelegateCommand AgendamentoTappedCommand =>
+            _agendamentoTappedCommand ?? (_agendamentoTappedCommand = new DelegateCommand(ExecuteAgendamentoTappedCommand));
+
+        async void ExecuteAgendamentoTappedCommand()
+        {
+            if (SelectAgendamento.Enviado || Aguarde)
+                return;
+
+            if (await _pageDialogService.DisplayAlertAsync("Agendamento", "Deseja reenviar esse agendamento?", "Sim", "Não"))
+            {
+                Aguarde = true;
+
+                if (await _agendamentoService.Salvar(SelectAgendamento))
+                {
+                    await _pageDialogService.DisplayAlertAsync("Agendamento", "Agendamento enviado com sucesso", "Ok");
+                    CarregarAgendamentosNaLista(_liteDatabase.GetCollection<Agendamento>());
+                }
+                else
+                {
+                    await _pageDialogService.DisplayAlertAsync("Agendamento", "Ocorreu um erro ao enviar o agendamento, tente novamente mais tarde.", "Ok");
+                }
+
+                Aguarde = false;
+
+            }
+        }
+
+
+        public ObservableCollection<Agendamento> MeusAgendamentos { get; set; }
+
+        private Agendamento _selectAgendamento;
+        public Agendamento SelectAgendamento
+        {
+            get { return _selectAgendamento; }
+            set { SetProperty(ref _selectAgendamento, value); }
+        }
+        private bool _aguarde;
+        public bool Aguarde
+        {
+            get { return _aguarde; }
+            set { SetProperty(ref _aguarde, value); }
+        }
+
 
         public override void OnAppearing()
         {
             var agendamentoDb = _liteDatabase.GetCollection<Agendamento>();
             if (MeusAgendamentos.Count != agendamentoDb.Count())
             {
-                MeusAgendamentos.Clear();
-                var veiculoDb = _liteDatabase.GetCollection<Veiculo>();
+                Aguarde = true;
 
-                var list = agendamentoDb.FindAll();
-
-                foreach (var item in list)
-                {
-                    item.Veiculo = veiculoDb.FindById(item.Veiculo.Id);
-                    MeusAgendamentos.Add(item);
-                }
+                CarregarAgendamentosNaLista(agendamentoDb);
+                
+                Aguarde = false;
 
             }
 
+        }
+
+        private void CarregarAgendamentosNaLista(ILiteCollection<Agendamento> agendamentoDb)
+        {
+            MeusAgendamentos.Clear();
+            var veiculoDb = _liteDatabase.GetCollection<Veiculo>();
+
+            var list = agendamentoDb.FindAll();
+
+            foreach (var item in list)
+            {
+                item.Veiculo = veiculoDb.FindById(item.Veiculo.Id);
+                MeusAgendamentos.Add(item);
+            }
         }
     }
 }
